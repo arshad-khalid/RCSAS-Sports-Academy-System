110b97589f2cdc69b2f543dbebb854057cbfa5b7
arshad-khalid <63362234+arshad-khalid@users.noreply.github.com>
2024-10-21 15:37:04 +0300

    Add files via upload

---
+++ src/main/java/assignment/rcsas/RcsasSoftware.java
@@ -0,0 +1,13 @@
+//ARSHAD KHALID NAZIR (TP058473)
+
+//Below code initiates the whole system
+package assignment.rcsas;
+
+public class RcsasSoftware {
+    public static void main(String[] args) {
+        UserTypeLogin utl = new UserTypeLogin();
+        utl.setVisible(true);
+        
+    }
+    
+}
