c11d390021fc1aec75ad265318021e762f88553b
arshad-khalid <63362234+arshad-khalid@users.noreply.github.com>
2024-10-21 15:24:59 +0300

    Add files via upload

---
+++ src/main/java/rslogin/Rslogin.java
@@ -0,0 +1,11 @@
+package rslogin;
+import java.io.*;
+public class Rslogin 
+{
+    public static void main(String[] args) 
+    {
+        smenu sm = new smenu();
+        sm.setVisible(true);
+    }
+    
+}
